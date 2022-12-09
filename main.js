52e2ddd3341357a267f841d90751096b0eab8a41
Usman Murtala <uzlash16@gmail.com>
2022-12-09 17:05:46 +0100

    added tailwind, beautify forms

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ function createWindow() {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
-        height: 600,
+        height: 700,
         backgroundColor: "#ccc",
         webPreferences: {
             nodeIntegration: true, // to allow require
--- renderer.js
+++ renderer.js
@@ -3,27 +3,19 @@
 // All of the Node.js APIs are available in this process.
 
 const { SerialPort } = require("serialport");
-const tableify = require("tableify");
 const fs = require("fs");
-
-/*Start Ports Functions*/
 
 async function listSerialPorts() {
   await SerialPort.list().then((ports, err) => {
     if (err) {
-      document.getElementById("error").textContent = err.message;
+      console.log("Error Listing Ports", err)
       return;
     } else {
-      document.getElementById("error").textContent = "";
+      console.log("Ports", ports)
     }
-    // console.log("ports", ports);
-
     if (ports.length === 0) {
-      document.getElementById("error").textContent = "No ports discovered";
+      console.log("No ports Discovered!")
     }
-
-    tableHTML = tableify(ports);
-    document.getElementById("ports").innerHTML = tableHTML;
   });
 }
 
@@ -32,15 +24,10 @@ function listPorts() {
   setTimeout(listPorts, 2000);
 }
 
-// Set a timeout that will check for new serialPorts every 2 seconds.
-// This timeout reschedules itself.
-
 setTimeout(listPorts, 2000);
 listSerialPorts();
 
-/*End Ports Functions*/
-
-const button = document.getElementById("saveconfig");
+const button = document.getElementById("saveConfig");
 
 button.addEventListener("click", () => {
   const host = document.forms[0].elements["host"].value;
@@ -58,6 +45,7 @@ button.addEventListener("click", () => {
       console.log("Error Creating Directory!", err);
     }
   });
+
   fs.writeFile("./config/naphimis.json", data, (err) => {
     if (!err) {
       console.log("File Written Successfully!", data);
